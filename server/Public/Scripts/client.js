42d40e312f6d3bdc89720ae3e2aee8e77bb60cd0
Austin Kammerer <austin4191@gmail.com>
2021-10-09 14:36:53 -0500

    client side input validation

--- server/Public/Scripts/client.js
+++ server/Public/Scripts/client.js
@@ -5,9 +5,68 @@ function pageLoad() {
   $(".operatorBtn").on("click", setOperation);
   // calculate the desired operation
   $("#equalBtn").on("click", calculate);
+  // clear inputs
+  //   $("clearBtn").on("click", clearInputs);
 }
 
 function setOperation() {
   let operation = $(this).data("operation");
   $("#equalBtn").data("operation", operation);
 }
+// POST function
+function calculate() {
+  console.log("=");
+  // validate inputs
+  let value1 = $("#value1Input").val();
+  console.log(value1);
+  let value2 = $("#value2Input").val();
+  console.log(value2);
+  let operation = $("#equalBtn").data("operation");
+  console.log(operation);
+  if (operation === "") {
+    alert("SELECT OPERATOR");
+  }
+  if (value1 === "") {
+    alert("ENTER VALUE 1");
+  }
+  if (value2 === "") {
+    alert("ENTER VALUE 2");
+  }
+
+  //   switch ("") {
+  //     case operation:
+  //       console.log("case1");
+
+  //       break;
+  //     case value1:
+  //       console.log("case2");
+
+  //       break;
+  //     case value2:
+  //       console.log("case3");
+
+  //       break;
+  // case value1 && value2:
+  //   alert("ENTER VALUES");
+  //   break;
+
+  //   break;
+
+  //   $.ajax({
+  //     method: "POST",
+  //     url: "/calc",
+  //     data: {
+  //       value1: value1,
+  //       value2: value2,
+  //       operation: operation,
+  //     },
+  //   }).then(function (res) {
+  //     getHistory();
+  //   });
+}
+
+// GET function
+
+// render function
+
+// clear inputs function
